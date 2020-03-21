ff12c73c52c041917905e29531a7c278606f7add
heaven <wjdtjdgh2005@gmail.com>
2020-03-21 22:20:31 +0900

    add algorithm solution file
    	add file:   "73. \353\213\244\353\246\254\353\245\274 \354\247\200\353\202\230\353\212\224 \355\212\270\353\237\255.py"

--- programers algorithm/test.py
+++ programers algorithm/test.py
@@ -1,7 +1,5 @@
-import numpy as np
-a = [12, 121, 1, 2]
-a.pop(0)
-for i in range(1, 1):
-	print(112121)
-# if a[] == np.array([]):
-# 	print(12121212)
+a = [11]
+if a != []:
+	print('y')
+else:
+	print('n')
