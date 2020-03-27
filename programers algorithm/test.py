1c25a9f38d15190ca6a972a5dcfab8346001280d
heaven <wjdtjdgh2005@gmail.com>
2020-03-27 21:05:58 +0900

    add algorithm solution file
    	add file:   "76. \353\254\270\354\236\220\354\227\264 \354\225\225\354\266\225.py"

--- programers algorithm/test.py
+++ programers algorithm/test.py
@@ -1,2 +1,14 @@
-a = 10.9
-print(int(a))
+li = ['a', 'a', 'b', 'b', 'a', 'c', 'c', 'c', '']
+cnt = 1
+ans = ''
+for i in range(len(li) - 1):
+    if li[i] == li[i + 1]:
+        cnt += 1
+    else:
+        if cnt != 1:
+            ans = ans + str(cnt) + li[i]
+            cnt = 1
+        else:
+            ans = ans + li[i]
+print(ans)
+
