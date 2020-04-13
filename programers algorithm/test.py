44a404ef9e70cabeb47ed5ee79fab2251cbd5825
heaven <wjdtjdgh2005@gmail.com>
2020-04-13 18:23:15 +0900

    add algorithm solution file
    	add file:   "85. \354\242\205\354\235\264\354\240\221\352\270\260.py"

--- programers algorithm/test.py
+++ programers algorithm/test.py
@@ -1,7 +1,3 @@
-import re
+a = [1, 2, 3, 4, 5]
 
-files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
-
-temp = [re.split(r"([0-9]+)", s) for s in files]
-
-print(temp)
+print(list(reversed(a)))
