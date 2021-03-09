89761c492ac5003d08212240e90250d57ecce0f5
heaven <wjdtjdgh2005@gmail.com>
2021-03-09 18:55:18 +0900

    add algorithm solution file 	renamed:    104test.py -> "104. \354\210\234\354\234\204 \352\262\200\354\203\211.py" 	new file:   "105. \353\260\251\353\254\270\352\270\270\354\235\264.py" 	modified:   Basic_algorithm/basic_function.py 	    add sum_list function

--- programers algorithm/Basic_algorithm/basic_function.py
+++ programers algorithm/Basic_algorithm/basic_function.py
@@ -2,6 +2,8 @@
 1. lower_bound
 
 2. upper_bound
+
+3. sum_list
 '''
 
 
@@ -23,5 +25,14 @@ def upper_bound(arr, target):
         else: end_ind = mid_ind
     return arr[start_ind] # return value
     # return start_ind      # return index
+
+
+def sum_list(lst, res = 0):
+    for i in lst:
+        if type(i) == list:
+            res += sum_list(i)
+        else:
+            res += i
+    return res
 
 
